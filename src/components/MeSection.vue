fc41d6caf2bfa7281a61894d8a76e8ccdb70f5ca
Twees <ptwees@icloud.com>
2026-01-06 17:47:13 -0500

    Update MeSection.vue

--- src/components/MeSection.vue
+++ src/components/MeSection.vue
@@ -1,36 +1,51 @@
 <template>
- <section class="relative w-full overflow-hidden min-h-screen
- flex items-center">
- <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-5xl
- lg:max-w-7xl mx-auto relative z-10">
- <div class="grid lg:grid-cols-2 gap-10 xl:gap-14 items-center">
-  <div class="flex flex-col md:mt-0 mt-20 justify-center">
-    <div class="text-white lg:text-left"
-    data-aos="fade-right"
+  <section
+    class="relative w-full overflow-hidden min-h-screen flex items-center"
+  >
+    <div
+      class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-5xl lg:max-w-7xl mx-auto relative z-10"
     >
-    <h1 class="text-white font-bold text-4xl
-    md:text-5xl lg:text-6xl leading-tight">
-    Hey, I'm <span
-    class="text-transparent bg-clip-text bg-linear-to-r
-    from-red-500 to-yellow-600"
-    >Ian 😀</span>
-    </h1>
+      <div class="grid lg:grid-cols-2 gap-10 xl:gap-14 items-center">
+        <div class="flex flex-col md:mt-0 mt-20 justify-center">
+          <div class="text-white lg:text-left" data-aos="fade-right">
+            <h1
+              class="text-white font-bold text-4xl md:text-5xl lg:text-6xl leading-tight"
+            >
+              Hey, I'm
+              <span
+                class="text-transparent bg-clip-text bg-linear-to-r from-red-500 to-orange-200"
+                >Ian </span
+              >😀
+            </h1>
+            <p
+              class="text-blue-200 font-medium text-lg md:text-xl mt-4"
+              data-aos="fade-right"
+              data-aos-delay="80"
+            ></p>
+            <p>Full-Stack Developer & Content Specialist</p>
+          </div>
+          <p
+            class="text-gray-300 pt-6 text-center lg:text-left mx-auto max-w-xl text-lg leading-relaxed"
+          >
+            I was eyes to the blind, and feet was I to the lame. 16I was a
+            father to the poor: and the cause which I knew not I searched out.
+            17And I brake the jaws of the wicked, and plucked the spoil out of
+            his teeth.
+          </p>
+          <div
+            class="flex items-center gap-4 pt-8 flex-col sm:flex-row sm:w-max sm:mx-auto lg:mx-0">
+            
+        </div>
+        </div>
+      </div>
     </div>
-
-  </div>
-
- </div>
-
- </div>
-
- </section>
+  </section>
 </template>
 
 <script>
 export default {
   name: "MeSection",
-}
+};
 </script>
 
-<style ></style>
-
+<style></style>
