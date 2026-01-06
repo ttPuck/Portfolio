9f884b38e73c31970d668b5f5cc5711c81591988
Twees <ptwees@icloud.com>
2026-01-05 23:33:42 -0500

    Nightly 1
    MeSection.vue creation, imported into App.vue and imported all preq and dep

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,41 @@
   <div class="min-h-screen bg-linear-to-br from-gray-900
   via-blue-900 to-gray-900">
 <NavBar />
+<MeSection />
  
 </div>
 </template>
 <script>
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import MeSection from './components/MeSection.vue';
 import NavBar from './components/NavBar.vue';
+import {ref, nextTick, onMounted } from 'vue';
+
 export default {
 components : {
     NavBar, 
+  MeSection,
+},
+setup() {
+  const loading = ref(true);
+  onMounted(async () => {
+    await nextTick();
+    Aos.init({
+      duration: 900,
+      once: false,
+      offest: 100,
+      easing: 'ease-in-out',
+    });
+setTimeout(() => {
+  loading.value = false;
+
+}, 800);
+  });
+  return {loading};
+
+
 }
-
-};
+}
 </script>
-<style >
-
-
-</style>
---
+++ src/components/MeSection.vue
@@ -0,0 +1,36 @@
+<template>
+ <section class="relative w-full overflow-hidden min-h-screen
+ flex items-center">
+ <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-5xl
+ lg:max-w-7xl mx-auto relative z-10">
+ <div class="grid lg:grid-cols-2 gap-10 xl:gap-14 items-center">
+  <div class="flex flex-col md:mt-0 mt-20 justify-center">
+    <div class="text-white lg:text-left"
+    data-aos="fade-right"
+    >
+    <h1 class="text-white font-bold text-4xl
+    md:text-5xl lg:text-6xl leading-tight">
+    Hey, I'm <span
+    class="text-transparent bg-clip-text bg-linear-to-r
+    from-red-500 to-yellow-600"
+    >Ian 😀</span>
+    </h1>
+    </div>
+
+  </div>
+
+ </div>
+
+ </div>
+
+ </section>
+</template>
+
+<script>
+export default {
+  name: "MeSection",
+}
+</script>
+
+<style ></style>
+
