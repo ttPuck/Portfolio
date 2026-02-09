2d00d3b975abc22c7c15e5f1e8a27ebc32c8be8a
Ian Garnett (Puck) <ttpuck@icloud.com>
2026-02-08 21:30:17 -0500

    Update LoadingSpinner.vue

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,19 +1,19 @@
 <template>
   <div
-    class="flex justify-center items-center h-screen bg-linear-to-r from-gray-900 via-blue-900 to-gray-900"
+    class="flex justify-center items-center h-screen bg-(--color-bg)"
   >
     <div class="text-center">
-      <div class="w-3 h-3 bg-blue-400 rounded-full animate-bounce"></div>
+      <div class="w-5 h-5 bg-(--color-primary) rounded-full animate-bounce"></div>
       <div
-        class="w-3 h-3 bg-blue-400 rounded-full animate-bounce"
+        class="w-5 h-5 bg-(--color-secondary) rounded-full animate-bounce"
         style="animation-delay: 0.1s"
       ></div>
       <div
-        class="w-3 h-3 bg-blue-400 rounded-full animate-bounce"
+        class="w-5 h-5 bg-(--color-primary) rounded-full animate-bounce"
         style="animation-delay: 0.2s"
       ></div>
       <div
-        class="w-3 h-3 bg-blue-400 rounded-full animate-bounce"
+        class="w-5 h-5 bg-(--color-secondary) rounded-full animate-bounce"
         style="animation-delay: 0.3s"
       ></div>
     </div>
